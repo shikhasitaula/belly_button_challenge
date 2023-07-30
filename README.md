2ceebce4e3f61773d9ec87f867dba336ad5251db
shikhasitaula <129104368+shikhasitaula@users.noreply.github.com>
2023-07-30 11:45:09 -0700

    Added content to the readme file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# belly_button_challenge
+# Overview
+## Belly Button Biodiversity Dashboard
+In this challenge, I have built an interactive dashboard to explore the Belly button Biodiversity dataset, which catalogs the microbes that colonize the human navels. The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+---
+
+**Steps taken to complete this analysis:**
+*  Used the d3 library to read in sample.json from the URL `https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json`
+* Created a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+  - First, used the select method in d3 library to select the html component to append the name and metadata" `let   selectOption = d3.select("#selDataset")`". Then, used d3.json to load data from the url and used `then` method to print the belly botton dataset in the console. Next, I retrieved all the names from the dataset, run the for loop and append them to the dropdown. `for ( let i = 0; i < names.length; i++){`
+        `selectOption.append("option").text(names[i]).property("value", names[i]);`
+    `}` Finally, plotted the bar chart ` Plotly.newPlot("bar", data)` 
+* Created a bubble chart:
+   - Used `otu_ids` for the x values,  `sample_values` for the y values,  `sample_values` for the marker size,  `otu_ids` for the marker colors, `otu_labels` for the text values
+  
+* Displayed the sample metadata, i.e., an individual's demographic information with its key-value pair from the metadata JSON object.
+* Deployed app to a free static page hosting service.
+
+
+1. README.md: Provides orientation for this challenge.
+2. static/js: Is has the logic for app.js
+3. index.html: It has the html file.
+4. samples.json: It is the json file for the reference. 
--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,19 @@
+// url containing the data.
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-
+//keeps track of what was selected.
 let selectOption = d3.select("#selDataset");
+// keeps track of what was selected.
 let selectDemographic = d3.select("#sample-metadata")
 let result;
+// Uses the D3 library to read in samples.json form the url
 const sample = d3.json(url).then(function(data){
+    // prints the belly button data in the console.
     console.log("Belly button samples: ", data)  
+    // just grabs name of the from the array.
     let names = data.names;
+    // loops through each names in the array 
+    // appends the selected name to the div
     for ( let i = 0; i < names.length; i++){
         selectOption.append("option").text(names[i]).property("value", names[i]);
     }
@@ -14,7 +21,7 @@ const sample = d3.json(url).then(function(data){
 
  });
 
-
+// function to plot the barchart.
  function barChart(sampleValues, otuIds, otuLabels){
     let data = [{
         x: sampleValues,
@@ -27,14 +34,14 @@ const sample = d3.json(url).then(function(data){
 
     Plotly.newPlot("bar", data)
  }
-
+// Fixes the marker size.
  function getMarkerSize(sampleValues){
     let size = sampleValues.map(function(data){
         return data / 2;
     })
     return size;
 }
-
+// creates the bubble chart.
  function bubblePlot(otuIds, sampleValues, otuLabels){
     let data = [{
         x: otuIds,
@@ -50,7 +57,7 @@ const sample = d3.json(url).then(function(data){
     }];
     Plotly.newPlot('bubble', data)
  };
-
+// appends id, ethnicity, gender , age location, bbtyoe and wfreq 
  function displayMetadata(metadata){
     selectDemographic.selectAll("*").remove();
     selectDemographic.append('span').text(`id : ${metadata.id}`);
@@ -67,25 +74,34 @@ const sample = d3.json(url).then(function(data){
     selectDemographic.append('br');
     selectDemographic.append('span').text(`wfreq : ${metadata.wfreq}`);
  }
-
+// this function runs if the change is made meaning, it runs if different name is clicked.
 function onOptionChange(value) {
     let samples = result.samples;
     let metadata = result.metadata;
+// uses filter method of the javascrip to return the name selected.
     let selected = samples.filter(function(data){
         return data.id === value;
     })[0]
+    // uses filter method to retrun the metadata of the selected name.
     let selectedMetadata = metadata.filter(function(data){
          return data.id === parseInt(value);
     })[0]
-    // console.log(filtered);
-    // let index = result.names.indexOf(value);
-    // let selected = samples[index];
+   // navigate to the selected sample values and returns top 10 otus' ids, sample value and otu labels.
     let topIds = selected.otu_ids.slice(0, 10)
         .map(function(otu) { return "OTU " + otu });
     let topValues = selected.sample_values.slice(0, 10);
     let otuLabel = selected.otu_labels.slice(0, 10);
+    // calls the function to display bar chart, bubble plot and display metadata.
     barChart(topValues.reverse(), topIds, otuLabel );
     bubblePlot( selected.otu_ids, selected.sample_values, otuLabel)
     displayMetadata(selectedMetadata)
 
 }
+
+
+
+
+// Alternative way of getting the selected names.
+   // console.log(filtered);
+    // let index = result.names.indexOf(value);
+    // let selected = samples[index];
